60465d169ec002ce2202fd535d760e2b591c08a0
Vass Norbert <vassnorbert20@gmail.com>
2025-01-20 11:58:35 +0100

    feat: adding light mode option

--- src/lib/components/Description.svelte
+++ src/lib/components/Description.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+	import { toggleMode } from 'mode-watcher';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import CodeXml from 'lucide-svelte/icons/code-xml';
 	import SquareArrowOutUpRight from 'lucide-svelte/icons/square-arrow-out-up-right';
 </script>
 
-<div class="prose mb-16 dark:prose-invert prose-p:text-justify">
-	<h1 class="text-center">Online file format converter</h1>
-	<p>
-		Easily convert between popular video, image, and audio file formats — all directly in your browser. For example,
-		quickly turn a large MKV video into a compact MP4, or a PNG into a JPEG. Your files stay secure since
-		conversions happen entirely client-side, with <strong>no usage limits!</strong>
-	</p>
-	<p>
-		The tool uses a
-		<a href="https://github.com/ffmpegwasm/ffmpeg.wasm" target="_blank">WebAssembly port of FFmpeg</a>
-		for all client-side conversions. The site itself is built with SvelteKit and styled using TailwindCSS.
-	</p>
-	<Button variant="outline" href="https://github.com/Plague-Doc/file-converter" class="no-underline" target="_blank">
-		<SquareArrowOutUpRight /> Check out the source code!
-	</Button>
+<div class="flex flex-col">
+	<div class="prose mb-6 dark:prose-invert prose-p:text-justify">
+		<h1 class="text-center">Online file format converter</h1>
+		<p>
+			Easily convert between popular video, image, and audio file formats — all directly in your browser. For
+			example, quickly turn a large MKV video into a compact MP4, or a PNG into a JPEG. Your files stay secure
+			since conversions happen entirely client-side, with <strong>no usage limits!</strong>
+		</p>
+		<p>
+			The tool uses a
+			<a href="https://github.com/ffmpegwasm/ffmpeg.wasm" target="_blank">WebAssembly port of FFmpeg</a>
+			for all client-side conversions. The site itself is built with SvelteKit and styled using TailwindCSS.
+		</p>
+	</div>
+
+	<div class="flex gap-4">
+		<Button variant="outline" onclick={toggleMode}>
+			<Moon class="hidden dark:block" />
+			<span class="hidden dark:block">Dark theme</span>
+			<Sun class="block dark:hidden" />
+			<span class="block dark:hidden">Light theme</span>
+		</Button>
+
+		<Button variant="outline" href="https://github.com/Plague-Doc/file-converter" class="w-fit" target="_blank">
+			<CodeXml /> Source code
+		</Button>
+
+		<Button disabled variant="outline" class="w-fit">
+			<SquareArrowOutUpRight /> Other projects
+		</Button>
+	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import { onMount } from 'svelte';
 	import { converter } from '$lib/store.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 
@@ -12,6 +13,7 @@
 	});
 </script>
 
-<main class="container mx-auto px-2 pt-16">
+<ModeWatcher />
+<main class="container mx-auto px-2 pb-8 pt-16">
 	{@render children()}
 </main>
