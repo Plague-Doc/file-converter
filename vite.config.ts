5b83a8356523b43fb096f4e11392650b5d110c18
Vass Norbert <vassnorbert20@gmail.com>
2025-01-25 00:36:08 +0100

    chore: cors cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-const viteServerConfig = {
-	name: 'viteServerConfig',
-	configureServer(server: any) {
-		server.middlewares.use((_req: any, res: any, next: any) => {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Methods', 'GET');
-			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-			next();
-		});
-	}
-};
-
 export default defineConfig({
-	plugins: [sveltekit(), viteServerConfig],
+	plugins: [sveltekit()],
 	optimizeDeps: {
 		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
 	},
